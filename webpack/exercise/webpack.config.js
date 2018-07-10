2f4845510d343fcd624128d347739371483cb23e
wangzhf <zhifengtech@gmail.com>
2018-07-10 22:31:32 +0800

    dev tool

--- webpack/exercise/webpack.config.js
+++ webpack/exercise/webpack.config.js
@@ -8,9 +8,18 @@ module.exports = {
         app: './src/index.js', 
         print: './src/print.js'
     }, 
+
+    // 
+    devtool: 'inline-source-map',
+
+    // devServer: {
+    //     contentBase: './dist'
+    // },
+
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'), 
+        publicPath: '/'
     }, 
     plugins: [
         new CleanWebpackPlugin(['dist']),
