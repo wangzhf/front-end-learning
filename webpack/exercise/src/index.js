37bae9b4a7a7b44355a622d4db5b7bcf2899f702
wangzhf <zhifengtech@gmail.com>
2018-07-10 21:58:08 +0800

    asset management

--- webpack/exercise/src/index.js
+++ webpack/exercise/src/index.js
@@ -1,8 +1,20 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './test.png';
+import Data from './data.xml';
 
 function component() {
     var element = document.createElement('div');
     element.innerHTML = _.join(['hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    // 添加图像
+    var myIcon = new Image();
+    myIcon.src = Icon;
+    //element.appendChild(myIcon);
+
+    console.log(Data);
+
     return element;
 }
 
