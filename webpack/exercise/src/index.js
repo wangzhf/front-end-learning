fc100a7119d4147be14dc6cbb90a091cd218744b
wangzhf <zhifengtech@gmail.com>
2018-07-10 23:56:51 +0800

    code splitting: dynamic imports async

--- webpack/exercise/src/index.js
+++ webpack/exercise/src/index.js
@@ -1,10 +1,8 @@
-function getComponent() {
-    // 在注释中使用了 webpackChunkName。这样做会导致我们的 bundle 被命名为 lodash.bundle.js
-    return import(/* webpackChunkName: 'lodash' */ 'lodash').then(_ => {
-        var element = document.createElement('div');
-        element.innerHTML = _.join(['hello', 'webpack'], ' ');
-        return element;
-    }).catch(error => 'An error occured while loading the component');
+async function getComponent() {
+    var element = document.createElement('div');
+    const _ = await import(/* webpackChunkName: 'lodash' */ 'lodash');
+    element.innerHTML = _.join(['hello', 'webpack'], ' ');
+    return element;
 }
 
 getComponent().then(component => {
