8dee62353c9db410d0c69c7c61b86e2c1f834170
wangzhf <zhifengtech@gmail.com>
2018-07-11 21:36:23 +0800

    lazy loading

--- webpack/exercise/src/index.js
+++ webpack/exercise/src/index.js
@@ -1,10 +1,21 @@
-async function getComponent() {
+import _ from 'lodash';
+
+function component() {
     var element = document.createElement('div');
-    const _ = await import(/* webpackChunkName: 'lodash' */ 'lodash');
+    var button = document.createElement('button');
+    var br = document.createElement('br');
+
+    button.innerHTML = 'Click me and look at the console!';
     element.innerHTML = _.join(['hello', 'webpack'], ' ');
+    element.appendChild(br);
+    element.appendChild(button);
+
+    button.onclick = e => import(/* webpackChunkName: "print"*/ './print').then(module => {
+        var print = module.default;
+        print();
+    });
+
     return element;
 }
 
-getComponent().then(component => {
-    document.body.appendChild(component);
-});
+document.body.appendChild(component());
---
+++ webpack/exercise/src/print.js
@@ -0,0 +1,5 @@
+console.log('The print.js module has loaded! See the network tab in dev tool...');
+
+export default () => {
+    console.log('Button Clicked: Here\'s "some text"!');
+}
