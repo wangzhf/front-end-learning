39272e2c5cda0fc39d3377d661b049efc0a4376e
wangzhf <zhifengtech@gmail.com>
2018-07-11 22:30:47 +0800

    progressive-web-application

--- webpack/exercise/src/index.js
+++ webpack/exercise/src/index.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-//import Print from './print';
+import Print from './print';
 
 function component() {
     var element = document.createElement('div');
@@ -13,3 +13,14 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+// 注册Service work
+if('serviceWorker' in navigator){
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+            console.log('Service Worker: ', registration);
+        }).catch(registration => {
+            console.log('Service worker registration failed: ', registration);
+        });
+    });
+}
--- webpack/exercise/webpack.dev.js
+++ webpack/exercise/webpack.dev.js
@@ -1,5 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'development',
@@ -7,5 +8,14 @@ module.exports = merge(common, {
 
     devServer: {
         contentBase: './dist'
-    }
+    }, 
+
+    plugins: [
+        new WorkboxPlugin.GenerateSW({
+            // 这些选项帮助 ServiceWorkers 快速启用, 
+            // 不允许遗留任何“旧的” ServiceWorkers
+            clientsClaim: true, 
+            skipWaiting: true
+        })
+    ]
 });
